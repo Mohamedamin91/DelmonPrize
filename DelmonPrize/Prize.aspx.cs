c02faaaaf02d7dfc2b211c821a5e2550ce49ef05
Mohammed Amin <m.amin@delmon.com.sa>
2023-01-22 15:11:21 +0300

    1.0

--- DelmonPrize/Prize.aspx.cs
+++ DelmonPrize/Prize.aspx.cs
@@ -27,12 +27,13 @@ namespace DelmonPrize
         SqlDataReader dr;
         SqlDataReader dr2;
         SqlDataReader dr3;
-
-       // int randomNumber = 0;
+        Random random = new Random();
+        int randomNumber = 0;
+        int CompanyID = 0;
         int min = 1;
         int max = 5;
         string companyname = "";
-        string fullname;
+        string fullname="";
         
         List<int> numbers = new List<int>();
         Random rand = new Random();
@@ -51,38 +52,34 @@ namespace DelmonPrize
         protected void btncheckWinner_Click(object sender, EventArgs e)
         {
             string Celebrationemoje = "\uD83C\uDF89";
-            string Sadnessemoje = "☹️";
             SqlParameter paramCompanyID = new SqlParameter("@C1", SqlDbType.Int);
             SqlParameter paramWinnerID = new SqlParameter("@C2", SqlDbType.Int);
-
-
-            try
-            {
-                Sqlconn.OpenConection();
-                while (numbers.Count < 5)
+          
+                try
                 {
-                    int randomNumber = rand.Next(1, 5);
+                  
+                    Sqlconn.OpenConection();
+                     //   min = Convert.ToInt32(dr["MIN"]);
+                    //   max = Convert.ToInt32(dr["MAX"]);
+                    randomNumber = random.Next(min, max+1);
                     paramWinnerID.Value = randomNumber;
-                    if (!numbers.Contains(randomNumber))
+                    dr2 = Sqlconn.DataReader("SELECT FullName,COMPName_EN, company from prize, Companies where prize.company=Companies.CRNumber and  [Gifts]=1   and [Selected]=0   and [Attended] =1  and CandID = @C2 ", paramWinnerID);
+                    if (dr2.HasRows)
                     {
-                        numbers.Add(randomNumber);
-                        dr2 = Sqlconn.DataReader("SELECT FullName ,COMPName_EN from prize,Companies where Companies.CRNumber = prize.Company and Gifts = 1 and Selected = 0 and Attended = 1 and CandID= " + randomNumber + " ");
-                        if (dr2.HasRows)
+                        while (dr2.Read())
                         {
-                            while (dr2.Read())
-                            {
-                                fullname = dr2["FullName"].ToString();
-                                companyname = dr2["COMPName_EN"].ToString();
-                            }
-                            lblMsg.Text = Celebrationemoje + " Congratulations  '" + fullname + "' ,  The Holder of Raffle Coupon Number.:  '" + randomNumber.ToString() + "' " + Celebrationemoje;
-                            lblMsg.Visible = true;
-                            Page.ClientScript.RegisterStartupScript(typeof(string), "fadeMsg", "fade('" + lblMsg.ClientID + "');", true);
-                            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\amin\Source\Repos\DelmonPrize\DelmonPrize\award.wav");
-                            //  player.Play();
-                            Sqlconn.ExecuteQueries("update prize set Selected = 1 where CandID=@C2 ", paramWinnerID);
+                            CompanyID = Convert.ToInt32(dr2["company"]);
+                            fullname = dr2["FullName"].ToString();
+                            companyname = dr2["COMPName_EN"].ToString();
+                            paramCompanyID.Value = CompanyID;
 
                         }
-
+                    Sqlconn.ExecuteQueries("update  prize set selected = 1 where  CandID = @C2 ", paramWinnerID);
+                    lblMsg.Text = Celebrationemoje + " Congratulations   '" + fullname + "' ,  The Holder of Raffle Coupon No.:  '" + randomNumber.ToString() + "' " + Celebrationemoje;
+                    Page.ClientScript.RegisterStartupScript(typeof(string), "fadeMsg", "fade('" + lblMsg.ClientID + "');", true);
+                    System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\amin\Source\Repos\DelmonPrize\DelmonPrize\award.wav");
+                    //  player.Play();
+             
                     }
                 }
 
@@ -90,29 +87,6 @@ namespace DelmonPrize
 
 
 
-                    
-
-                          
-                       
-
-
-                    
-
-
-
-
-                } 
-                
-
-
-
-                //else
-                //{
-                //    lblMsg.Text = string.Empty;
-                //    lblMsg2.Text = "Sorry, there are no more candidates available " + Sadnessemoje;
-                //    Page.ClientScript.RegisterStartupScript(typeof(string), "fadeMsg", "fade('" + lblMsg2.ClientID + "');", true);
-
-                //}
 
 
 
@@ -121,23 +95,35 @@ namespace DelmonPrize
 
 
 
-               
+
+
+
+
+
+
+
+
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    Sqlconn.CloseConnection();
+                }
+
+
+
+
             
 
 
 
 
-            catch (Exception ex)
-            {
-                lblMsg2.Text = ex.ToString();
-            
-            }
-            finally
-            {
 
-                Sqlconn.CloseConnection();
-            }
-            Page.DataBind();
+
+
         }
     }
 }
